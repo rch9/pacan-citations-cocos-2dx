a15cad4a7dc362288da4d5313857157d682f34e2
rch_9 <enlofpoliteh121@gmail.com>
2016-10-31 14:24:05 +0300

    added loacalization

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -19,7 +19,12 @@ bool HelloWorld::init() {
     }
     _isClicked = false;
     _currentIndex = 0;
-    cocostudio::JsonLocalizationManager::getInstance()->initLanguageData("text/ru-RU.lang.json");
+
+    if (Application::getInstance()->getCurrentLanguage() == LanguageType::RUSSIAN) {
+        cocostudio::JsonLocalizationManager::getInstance()->initLanguageData("text/ru-RU.lang.json");
+    } else {
+        cocostudio::JsonLocalizationManager::getInstance()->initLanguageData("text/en-US.lang.json");
+    }
 
     initPageView();
 
@@ -40,13 +45,23 @@ void HelloWorld::initPageView() {
     pageView->setPosition(visibleSize / 2.f);
     pageView->removeAllItems();
     pageView->setIndicatorEnabled(false);
-//    pageView->setScrollDuration(0.3);
 
-    int pageCount = 29;
+    auto localManager = cocostudio::JsonLocalizationManager::getInstance();
+
+
+    auto l1 = Layout::create();
+    l1->setContentSize(size);
+    auto hi = Label::createWithTTF(localManager->getLocalizationString("hi"), "fonts/helios.ttf", 65);
+    hi->setAnchorPoint(Vec2::ANCHOR_MIDDLE);
+    hi->setPosition(400, 700);
+    hi->setWidth(700);
+    hi->setAlignment(TextHAlignment::LEFT);
+    l1->addChild(hi);
+    pageView->pushBackCustomItem(l1);
+
+    int pageCount = 30;
     for (int i = 0; i < pageCount; ++i) {
         auto layout = TouchableLabel::create(convertInt(i).c_str(), convertJson(i, false).c_str(), convertJson(i, true).c_str());
-
-//        fillLayout(layout, i);
 
         pageView->pushBackCustomItem(layout);
     }
@@ -65,7 +80,7 @@ void HelloWorld::fillLayout(Layout *layout, const int& ind) {
 
 std::string HelloWorld::convertInt(const int &ind) {
     char buffer[10];
-    sprintf(buffer, "img/%d.jpg", ind + 1);
+    sprintf(buffer, "img/%d.jpg", ind);
 
     return std::string(buffer);
 }
@@ -74,9 +89,9 @@ std::string HelloWorld::convertJson(const int &ind, bool flag) {
 
     char buffer[3];
     if (flag) {
-        sprintf(buffer, "%da", ind + 1);
+        sprintf(buffer, "%da", ind);
     } else {
-        sprintf(buffer, "%d", ind + 1);
+        sprintf(buffer, "%d", ind);
     }
 
     return std::string(buffer);
